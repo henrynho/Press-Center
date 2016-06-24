4f471dc39e19d289f060105f609aade1559870a9
Henry Hilario <henrynho.hilario@gmail.com>
2016-06-24 18:44:48 -0500

    nueva web

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	var altura = $(".navegador").offset().top;
+	$(window).on("scroll",function(){
+		if($(window).scrollTop()> altura){
+			$(".navegador").addClass("menu-fixed");
+		}else{
+			$(".navegador").removeClass("menu-fixed");
+		}
+	});
+});
